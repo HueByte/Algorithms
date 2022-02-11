2cd09032230daace04f650334accc885fcb706e0
HueByte <ainodeve@gmail.com>
2022-02-11 22:26:27 +0100

    cleaning

--- c#/BubbleSort/Program.cs
+++ c#/BubbleSort/Program.cs
@@ -1,22 +1,23 @@
-﻿var data = File.ReadAllLines("../../Data/1k.txt");
+﻿// load data from file
+var data = File.ReadAllLines("../../Data/1k.txt");
+
+// convert to int[]
 var input = Array.ConvertAll(data, int.Parse);
 
-int[] testData = input.Take(10).ToArray();
-
+// Bubble sort
 for (int i = 0; i < input.Length; i++)
 {
     for (int j = 0; j < input.Length - 1; j++)
     {
-        int z = 0;
         if (input[j] > input[j + 1])
         {
             // swap
-            z = input[j];
+            int temp = input[j];
             input[j] = input[j + 1];
-            input[j + 1] = z;
+            input[j + 1] = temp;
         }
     }
 }
 
-
-foreach (var item in input.Distinct()) Console.WriteLine(item);
+// output data
+foreach (var item in input) Console.WriteLine(item);
